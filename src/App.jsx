d311122bc03d2fd5de6538f6ec33e272136bb416
freetimezp <kraftukrnet@ukr.net>
2024-06-22 21:08:32 +0300

    02. js-portfolio-98-benvix. Main css. Components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Teams from "./components/teams/Teams";
+import Project from "./components/project/Project";
+import Testimonial from "./components/testimonial/Testimonial";
+import Blog from "./components/blog/Blog";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+
 function App() {
 
   return (
     <>
-      Start
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Teams />
+      <Project />
+      <Testimonial />
+      <Blog />
+      <Contact />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/teams/Teams.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./teams.css";
+
+const Teams = () => {
+    return (
+        <div>
+            Teams
+        </div>
+    );
+}
+
+export default Teams;
