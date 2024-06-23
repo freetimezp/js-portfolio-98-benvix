9c1aa141e791f713c0810c4e824baf797176f073
freetimezp <kraftukrnet@ukr.net>
2024-06-23 22:30:50 +0300

    05. js-portfolio-98-benvix. Header Component.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import './header.css';
+import { Link } from 'react-scroll';
+
+import { hero_image } from '../../assets';
 
 const Header = () => {
     return (
         <header className='header' id='header'>
             <div className="container full__height">
-
+                <div className="row">
+                    <div className="column">
+                        <h1 className="title">
+                            Building Dreams, {" "}
+                            <span className="g-text">Creative Reality</span>
+                        </h1>
+                        <p className="text__muted description">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Repudiandae labore laudantium rerum quam, dolor explicabo
+                            obcaecati veniam quaerat est! Expedita aperiam officiis
+                            sed cumque nemo dolores placeat porro distinctio ratione
+                            fugiat, suscipit eveniet voluptatibus accusamus.
+                        </p>
+                        <div className="buttons__container">
+                            <Link to="services" smooth={true} className='btn'>
+                                Our Services
+                            </Link>
+                            <Link to="contact" smooth={true} className='btn btn__primary'>
+                                Contact Us
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="image__container">
+                            <img src={hero_image} alt="hero_image" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </header>
     );
