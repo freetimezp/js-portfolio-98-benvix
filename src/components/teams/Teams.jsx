ba8bf73b6d74a0995c02705db97e92a023cd367e
freetimezp <kraftukrnet@ukr.net>
2024-06-27 19:12:18 +0300

    09. js-portfolio-98-benvix. Teams Component.

--- src/components/teams/Teams.jsx
+++ src/components/teams/Teams.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import "./teams.css";
 
+import { teams } from '../../data';
+
 const Teams = () => {
     return (
-        <div>
-            Teams
-        </div>
+        <section id="team">
+            <div className="container">
+                <h1 className="title">Our <span className='g-text'>Teams</span></h1>
+
+                <h3 className="sub__title">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa
+                    architecto corrupti voluptates ipsam quos possimus asperiores
+                    veniam rerum cumque quidem.
+                </h3>
+
+                <div className="teams__container">
+                    {teams.map((team, index) => (
+                        <div className="team__card" key={index}>
+                            <div className="profile__container">
+                                <img src={team.profile} alt="profile" />
+                            </div>
+                            <div className="details">
+                                <h3 className="name">{team.name}</h3>
+                                <p className="text__muted">{team.title}</p>
+
+                                <div className="social__container">
+                                    {team.social.map((item, i) => (
+                                        <a
+                                            href={item.url || "#"}
+                                            className='icon__container'
+                                            key={i}
+                                            target='_blank'
+                                        >
+                                            {item.icon}
+                                        </a>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
     );
 }
 
