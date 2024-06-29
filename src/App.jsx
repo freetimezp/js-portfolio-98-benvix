33443251dff716b0916ee049f80b3f7cc4414362
freetimezp <kraftukrnet@ukr.net>
2024-06-29 11:08:48 +0300

    14. js-portfolio-98-benvix. Footer Component.

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       <Testimonial />
       <Blog />
       <Contact />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import "./footer.css";
 
+import Logo from '../logo/Logo';
+import { footer } from '../../data';
+import { Link } from 'react-scroll';
+
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer id='footer' className='footer'>
+            <div className="container">
+                <div className="row">
+                    <div className="column">
+                        <Logo />
+                        <form className="newsletter">
+                            <h3>Get weekly update</h3>
+                            <div className="control__container">
+                                <input type="email" placeholder='Enter your email address' className='control' />
+                                <button className="btn" type='button'>Subscribe</button>
+                            </div>
+                        </form>
+                    </div>
+
+                    {footer.map((list, index) => (
+                        <div className="column" key={index}>
+                            <div className="routes__name">{list.name}</div>
+                            <div className="routes__container">
+                                {list.routes.map((route, i) => (
+                                    <Link key={i} to={route.id} className='route__item'>
+                                        <p className="name">{route.name}</p>
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="copyright">
+                <div className="container">
+                    <h3>Copyright &copy; All Rights Reserved. - | 2024</h3>
+                    <p className="text__muted">Build with Love by Freetime.</p>
+                </div>
+            </div>
+        </footer>
     );
 }
 
